eed6d897ab45d5da37c80a8607b671a6ee6758ca
Joao Victor Pereira Santos <joaovictorsantoscpv@gmail.com>
2019-10-31 15:24:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
